252bcb2a3f75f3e987e8a414cc91447641dfce80
Vadim Mishkhozhev <vadim.stug@gmail.com>
2024-11-01 11:40:14 +0200

    Remove draggable

--- src/widgets/BeadWithLibrary/ui/BeadWithLibrary/BeadWithLibrary.tsx
+++ src/widgets/BeadWithLibrary/ui/BeadWithLibrary/BeadWithLibrary.tsx
@@ -1,5 +1,3 @@
-import KonvaCore from "konva/lib/Core";
-import type { KonvaEventObject } from "konva/lib/Node";
 import type { Stage as StageInstance } from "konva/lib/Stage";
 import { useEffect, useRef } from "react";
 import { Group, Layer, Stage } from "react-konva";
@@ -12,8 +10,6 @@ import { AxisY } from "../AxisY/AxisY";
 import { BeadSquare } from "../BeadSquare/BeadSquare";
 import { Rule } from "../Rule/Rule";
 
-KonvaCore.hitOnDragEnabled = true;
-
 interface BeadWithLibraryProps {
   rows: number;
   columns: number;
@@ -23,24 +19,6 @@ interface BeadWithLibraryProps {
 
 const axisXHeight = 30;
 const axisYWidth = 20;
-const scaleBy = 1.01;
-let lastCenter: { x: number; y: number } | null = null;
-let lastDist = 0;
-let dragStopped = false;
-
-function getDistance(
-  p1: { x: number; y: number },
-  p2: { x: number; y: number }
-) {
-  return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
-}
-
-function getCenter(p1: { x: number; y: number }, p2: { x: number; y: number }) {
-  return {
-    x: (p1.x + p2.x) / 2,
-    y: (p1.y + p2.y) / 2,
-  };
-}
 
 export const BeadWithLibrary = (props: BeadWithLibraryProps) => {
   const refStage = useRef<StageInstance>(null);
@@ -65,124 +43,8 @@ export const BeadWithLibrary = (props: BeadWithLibraryProps) => {
     }
   }, [viewBoxWidth, width]);
 
-  const handleWheel = (e: KonvaEventObject<WheelEvent>) => {
-    e.evt.preventDefault();
-    const stage = refStage.current;
-    if (stage) {
-      const oldScale = stage.scaleX();
-      const pointer = stage.getPointerPosition();
-
-      if (pointer) {
-        const mousePointTo = {
-          x: (pointer.x - stage.x()) / oldScale,
-          y: (pointer.y - stage.y()) / oldScale,
-        };
-
-        let direction = e.evt.deltaY > 0 ? 1 : -1;
-
-        if (e.evt.ctrlKey) {
-          direction = -direction;
-        }
-
-        const newScale =
-          direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
-        stage.scale({ x: newScale, y: newScale });
-
-        const newPos = {
-          x: pointer.x - mousePointTo.x * newScale,
-          y: pointer.y - mousePointTo.y * newScale,
-        };
-        stage.position(newPos);
-      }
-    }
-  };
-
-  const handleTouchMove = (e: KonvaEventObject<TouchEvent>) => {
-    e.evt.preventDefault();
-    const stage = refStage.current;
-
-    if (stage) {
-      const touch1 = e.evt.touches[0];
-      const touch2 = e.evt.touches[1];
-
-      // we need to restore dragging, if it was cancelled by multi-touch
-      if (touch1 && !touch2 && !stage.isDragging() && dragStopped) {
-        stage.startDrag();
-        dragStopped = false;
-      }
-
-      if (touch1 && touch2) {
-        // if the stage was under Konva's drag&drop
-        // we need to stop it, and implement our own pan logic with two pointers
-        if (stage.isDragging()) {
-          dragStopped = true;
-          stage.stopDrag();
-        }
-
-        const p1 = {
-          x: touch1.clientX,
-          y: touch1.clientY,
-        };
-        const p2 = {
-          x: touch2.clientX,
-          y: touch2.clientY,
-        };
-
-        if (!lastCenter) {
-          lastCenter = getCenter(p1, p2);
-          return;
-        }
-        const newCenter = getCenter(p1, p2);
-
-        const dist = getDistance(p1, p2);
-
-        if (!lastDist) {
-          lastDist = dist;
-        }
-
-        // local coordinates of center point
-        const pointTo = {
-          x: (newCenter.x - stage.x()) / stage.scaleX(),
-          y: (newCenter.y - stage.y()) / stage.scaleX(),
-        };
-
-        const scale = stage.scaleX() * (dist / lastDist);
-
-        stage.scaleX(scale);
-        stage.scaleY(scale);
-
-        // calculate new position of the stage
-        const dx = newCenter.x - lastCenter.x;
-        const dy = newCenter.y - lastCenter.y;
-
-        const newPos = {
-          x: newCenter.x - pointTo.x * scale + dx,
-          y: newCenter.y - pointTo.y * scale + dy,
-        };
-
-        stage.position(newPos);
-
-        lastDist = dist;
-        lastCenter = newCenter;
-      }
-    }
-  };
-
-  const handleTouchEnd = () => {
-    lastDist = 0;
-    lastCenter = null;
-  };
-
   return (
-    <Stage
-      ref={refStage}
-      width={width}
-      height={viewBoxHeight}
-      draggable
-      onWheel={handleWheel}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
+    <Stage ref={refStage} width={width} height={viewBoxHeight}>
       <Layer>
         <Group x={viewBoxWidth / 2}>
           <Group x={-viewBoxWidth / 2}>

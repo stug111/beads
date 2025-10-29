ec87904b11ed719b3ded40ce0447a24dd6acc5d4
Vadim Mishkhozhev <vadim.stug@gmail.com>
2025-10-29 12:27:45 +0200

    feat: enhance color selection components with active color state management

--- src/entities/bead/ui/settings/color-list.tsx
+++ src/entities/bead/ui/settings/color-list.tsx
@@ -1,9 +1,10 @@
 import { Box, Flex } from "@radix-ui/themes";
 import { useSignal } from "../../lib/signals";
-import { changeColor, colorPalette } from "../../model/store";
+import { changeColor, color, colorPalette } from "../../model/store";
 
 export function ColorList() {
   const colors = useSignal(colorPalette);
+  const activeColor = useSignal(color);
 
   const handleColorClick = (color: string) => () => {
     changeColor(color);
@@ -16,7 +17,8 @@ export function ColorList() {
           key={color}
           width="24px"
           height="24px"
-          className="rounded"
+          data-active={activeColor === color}
+          className="rounded data-[active=true]:ring-2 data-[active=true]:ring-offset-2 data-[active=true]:ring-gray-400 cursor-pointer"
           style={{ backgroundColor: color }}
           onClick={handleColorClick(color)}
         />
--- src/entities/bead/ui/settings/color-picker.tsx
+++ src/entities/bead/ui/settings/color-picker.tsx
@@ -1,12 +1,20 @@
 import type { ChangeEvent } from "react";
-import { changeColor } from "../../model/store";
+import { changeColor, color } from "../../model/store";
+import { useSignal } from "../../lib/signals";
 
 export function ColorPicker() {
+  const activeColor = useSignal(color);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     changeColor(e.target.value);
   };
 
   return (
-    <input type="color" className=" bg-white border border-gray-300 rounded cursor-pointer" onChange={handleChange} />
+    <input
+      type="color"
+      value={activeColor}
+      className=" bg-white border border-gray-300 rounded cursor-pointer"
+      onChange={handleChange}
+    />
   );
 }
